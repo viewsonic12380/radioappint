8246d733bb6809604d0aad2d33db026c47ebc7a4
view123 <view123@mail.ru>
2022-12-06 21:58:40 +0500

    Add class Detail

---
+++ src/main/java/org/example/model/Detail.java
@@ -0,0 +1,41 @@
+package org.example.model;
+
+public class Detail {
+    private Integer id;
+    private String name;
+    private Integer cost;
+
+    public boolean isNew() {
+        return id == null;
+    }
+
+    public Detail(Integer id, String name, Integer cost) {
+        this.id = id;
+        this.name = name;
+        this.cost = cost;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getCost() {
+        return cost;
+    }
+
+    public void setCost(Integer cost) {
+        this.cost = cost;
+    }
+}
